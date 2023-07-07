d07ae42b7d1c0b2f944ed343c1240965051d069f
Tiffani <tiffani.marie.watson@gmail.com>
2023-07-07 12:05:17 -0500

    added scanner to read inputs from the user and added  imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter Something: ");
+        String input = scanner.nextLine();
+
+        System.out.println("You Entered: \"" + input + "\"");
+
+        if (StringUtils.isNumeric(input)) {
+            System.out.println("\"" + input + "\" is a number");
+        }
+    }
 }
